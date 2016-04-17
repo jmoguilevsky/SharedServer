8bec672d779715f5015c620dfa654cf39e1e9822
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-04-17 17:40:26 -0300

    indexjs

--- index.js
+++ index.js
@@ -2,20 +2,7 @@ var pg = require('pg');
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-/*var bodyParser  = require("body-parser"),
-var methodOverride = require("method-override");
-var server   = http.createServer(app)
-*/
-/*
-var conString = "postgres://YourUserName:YourPassword@localhost:5432/YourDatabase";
-var client = new pg.Client(conString);
-client.connect();
-*/
-/*
-app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(bodyParser.json());  
-app.use(methodOverride());
-*/
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -23,10 +10,6 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
-/*app.get('/', function(request, response) {
-  response.send(process.env.DATABASE_URL);
-});*/
 
 app.get('/cool', function(request, response) {
   response.send(cool());
