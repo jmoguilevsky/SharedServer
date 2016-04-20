9d971fdcbe8a7b502d0b26d25f06fbfe4877809b
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-04-20 00:41:25 -0300

    fix gets.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var pg = require('pg');
 var cool = require('cool-ascii-faces');
 var express = require('express');
-var gets = require('./gets');
+//var gets = require('./gets');
 var app = express();
 var router = express.Router();
 
@@ -50,5 +50,21 @@ app.get('/users', function (request, response) {
 })
 
 */
+
+var getAllUsers = function (request,response){
+		var query = "select row_to_json(row(nombre,alias)) from usuario;";
+		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+			client.query(query, function(err, result) {
+				done();
+				if (err){ 
+					console.error(err); response.send("Error " + err); 
+				} else {
+					response.send(result.rows) ;
+					/*response.render('pages/db', {results: result.rows} );*/ 
+				}
+			});
+		});
+	}
+
 
 router.route('/users').get(getAllUsers);
