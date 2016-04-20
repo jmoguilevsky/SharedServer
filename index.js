e5d56c20e78e5f2eff8af01b1cf43046883901e9
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-04-20 00:25:36 -0300

    indexjs error

--- index.js
+++ index.js
@@ -33,23 +33,7 @@ app.get('/db', function (request, response) {
     });
   });
 })
-/*
-app.get('/users', function (request, response) {
-    pg.defaults.ssl = true;
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM usuario', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  })
-})
 
-*/
-
-router.route('/users').get(getAllUsers);
 module.exports = function() {
 
 	function getAllUsers(request,response){
@@ -69,4 +53,21 @@ module.exports = function() {
 	return{
 		getAllUsers: getAllUsers
 	}
-};
+}();
+/*
+app.get('/users', function (request, response) {
+    pg.defaults.ssl = true;
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM usuario', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  })
+})
+
+*/
+
+router.route('/users').get(getAllUsers);
