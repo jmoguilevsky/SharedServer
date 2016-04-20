48c289e9f98e3efd2c3bac8b6857472a49aa4f24
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-04-20 00:39:13 -0300

    fix gets.js

--- gets.js
+++ gets.js
@@ -6,14 +6,16 @@ module.exports = function() {
 		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 			client.query(query, function(err, result) {
 				done();
-				if (err)
-					{ console.error(err); response.send("Error " + err); }
-				else
-				{
+				if (err){ 
+					console.error(err); response.send("Error " + err); 
+				} else {
 					response.send(result.rows) ;
 					/*response.render('pages/db', {results: result.rows} );*/ 
 				}
 			});
 		});
 	}
+	return{
+		getAllUsers: getAllUsers
+	};
 }();
