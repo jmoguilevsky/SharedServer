3668376441e68d32352fd3e88f9558cff52fe0ff
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-04-20 00:18:20 -0300

    indexjs error

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/users', function (request, response) {
 
 */
 
-function getAllUser(request,response){
+var getAllUser = function (request,response){
 	var query = "select row_to_json(row(nombre,alias)) from usuario;";
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query(query, function(err, result) {
@@ -62,5 +62,5 @@ function getAllUser(request,response){
        /*response.render('pages/db', {results: result.rows} );*/ 
    		}
     });
-}
+};
 router.route('/users').get(getAllUser)
