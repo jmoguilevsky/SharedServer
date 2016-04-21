54e1fbcbeb3d92efb172b2f6d2fa04be888e7367
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-04-20 21:30:50 -0300

    try router de vuelta

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var pg = require('pg');
 var cool = require('cool-ascii-faces');
 var express = require('express');
-var gets = require('./gets');
+//var gets = require('./gets');
 var app = express();
 var router = express.Router();
+
+function getAllUsers(request,response){
+	var query = "select row_to_json(usuario) from usuario;";
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query(query, function(err, result) {
+			done();
+			if (err){ 
+				console.error(err); response.send("Error " + err); 
+			} else {
+				response.send(result.rows) ;
+			}
+		});
+	});
+}	
 
 app.set('port', (process.env.PORT || 5000));
 
