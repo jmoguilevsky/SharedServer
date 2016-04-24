f736ac7d62eed8bddba38f9c556d99041a653f69
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-04-24 20:25:57 -0300

    try posts

--- tests/requestsLikeAPro.py
+++ tests/requestsLikeAPro.py
@@ -1,6 +1,35 @@
 import requests
+import json
+
 res = requests.get("https://enigmatic-depths-58073.herokuapp.com/users")
 print res.headers
 print
 print res.text
 print
+
+str="""{
+  "user": { 
+    "name": "usuario",
+    "alias": "not a user",
+    "email": "usuario@usuario.com",
+    "interests": [{
+    	"category": "music/band",
+    	"value": "radiohead"
+      }, {
+    	"category": "music/band",
+    	"value": "pearl jam"
+      },{
+    	"category": "outdoors",
+    	"value": "running"
+    }],
+    "location": { 
+         "latitude": -121.45356,
+         "longitude": 46.51119
+    }
+  },
+  "metadata": {
+        "version": "0.1"
+  }
+}"""
+
+r = requests.post('https://enigmatic-depths-58073.herokuapp.com/users', data=json.dumps(str), headers={'content-type': 'application/json'});
