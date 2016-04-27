792745455ae97cc5f705473d62eb62790c3e1480
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-04-27 00:51:11 -0300

    post de un user

--- posts.js
+++ posts.js
@@ -31,7 +31,7 @@ module.exports = function() {
                     if (err) return rollback(client, done, err);
                     client.query(selectLastUser, function(err, result) {
                         if (err) return rollback(client, done);
-                        console.log('New User Id' + result.rows);
+                        console.log('New User Id\n' + result);
                         idUser = result.rows[0]['id'];
 
                         var interestsInserts = '';
