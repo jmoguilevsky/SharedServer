7a8ced2d8ac11b2d11fe8c32f5a4838ca30c5a3e
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-04-28 00:39:31 -0300

    comienzo modularizacion de querys

--- querys.js
+++ querys.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-    var getAllUsers = function() {
+    function getAllUsers() {
         return "SELECT array_to_json(array_agg(row_to_json(users_json))) as users from (select *, (SELECT array_to_json(array_agg(row_to_json(interests))) from (select category, value from Interest where idUser = id) interests ) AS Interests from \"USER\") as users_json;";
     }
 
