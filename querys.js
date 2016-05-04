3b5ce87076ddaec4766e1c2fa277cbd2c2b49f25
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-03 23:38:41 -0300

    querys de get con la nueva estructura

--- querys.js
+++ querys.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     function getAllUsers() {
-        return "SELECT array_to_json(array_agg(row_to_json(users_json))) as users from (select *, (SELECT array_to_json(array_agg(row_to_json(interests))) from (select category, value from Interest, UserInterest where UserInterest.idInterest = Interest.id and UserInterest.idUser = id) interests ) AS Interests from UserProfile) as users_json;";
+        return "SELECT array_to_json(array_agg(row_to_json(users_json))) as users from (select *, (SELECT array_to_json(array_agg(row_to_json(interests))) from (select category, value from Interest, UserInterest where UserInterest.idInterest = Interest.id and UserInterest.idUser = UserProfile.id) interests ) AS Interests from UserProfile) as users_json;";
     }
 
     return {
