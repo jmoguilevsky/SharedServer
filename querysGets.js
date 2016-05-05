b6de26026ad21b45d50006faf519229f610647ed
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-05 02:53:52 -0300

    query de post y refactoring

---
+++ querysGets.js
@@ -0,0 +1,19 @@
+module.exports = function() {
+    function getAllUsers() {
+        return "SELECT array_to_json(array_agg(row_to_json(users_json))) as users from (select *, (SELECT array_to_json(array_agg(row_to_json(interests))) from (select category, value from Interest, UserInterest where UserInterest.idInterest = Interest.id and UserInterest.idUser = UserProfile.id) interests ) AS Interests from UserProfile) as users_json;";
+    }
+
+    function selectInterest(interest) {
+        return 'select id from Interest where value =\'' + interest.value + '\' and category = \'' + interest.category + '\';'
+    }
+
+    function getUserId(user) {
+        return 'SELECT id from UserProfile where email = \'' + user.email + '\' ;';
+    }
+
+    return {
+        getAllUsers: getAllUsers,
+        getInterest: selectInterest,
+        getUser: selecttUser
+    }
+}();
---
+++ querysPosts.js
@@ -0,0 +1,43 @@
+module.exports = function() {
+
+    function queryInsertUserWithInterests(user) {
+        var newUserVariable = 'idNewUser';
+        var interests = '';
+        user.interests.forEach(function(interest) {
+            interests += queryInsertInterestForUser(interest, newUserVariable);
+        })
+
+        return 'DO $$' +
+            'DECLARE ' + newUserVariable + ' int;' +
+            'BEGIN ' +
+            queryInsertUser(user) + ' RETURNING id into ' + newUserVariable + ';' +
+            interests +
+            'END $$;';
+    }
+
+    function queryInsertInterestForUser(interest, idNewUser) {
+        return 'DECLARE idInterest int;' +
+            'BEGIN ' +
+            'select id from Interest where value =\'' + interest.value + '\' and category = \'' + interest.category + '\' into idInterest;' +
+            'IF idInterest is NULL THEN ' +
+            'insert into Interest(category, value) values(\'' + interest.category + '\',\'' + interest.value + '\') returning id into idInterest;' +
+            'END IF;' +
+            queryInsertUserInterest(idNewUser, 'idInterest') +
+            'END;';
+    }
+
+    function queryInsertUserInterest(idUser, idInterest) {
+        return 'INSERT INTO UserInterest (idUser, idInterest) values (' + idUser + ', ' + idInterest + ');'
+    }
+
+    function queryInsertUser(user) {
+        return 'INSERT INTO UserProfile(name, alias, email, latitude, longitude) ' +
+            'VALUES (\'' + user.name + '\', \'' + user.alias + '\', \'' + user.email + '\',' +
+            user.location.latitude + ',' + user.location.longitude + ')';
+    }
+
+    return {
+        insertUserWithInterest: queryInsertUserWithInterests,
+        insertUserInterest: queryInsertUserInterest
+    }
+}();
