5007819d1d6985f8e6c5a711a0c3fd4418a8baeb
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-05 01:16:54 -0300

    querys de post mande todo a la mierda y use una unica query tipo sp

--- posts.js
+++ posts.js
@@ -2,16 +2,34 @@ module.exports = function() {
     var pg = require('pg');
     var version = 0.1;
 
+    function queryInsertUserWithInterests(user) {
+        var newUserVariable = 'idNewUser';
+        var interests = '';
+        user.interests.forEach(function(interest) {
+            interests += queryInsertInterestForUser(interest, newUserVariable);
+        })
+
+        return 'DO $$' +
+            'DECLARE ' + newUserVariable + ' int;' +
+            'begin' +
+            queryInsertUser(user) + ' returning id into ' + newUserVariable + ';' +
+            interests +
+            'END $$;';
+    }
+
     function querSelectInterest(interest) {
         return 'select id from Interest where value =\'' + interest.value + '\' and category = \'' + interest.category + '\';'
     }
 
-    function queryInsertInterestForUser(interest, idUser) {
-        return 'DECLARE id'+interest.category+interest.value+' int; BEGIN ' +
-            'INSERT INTO Interest (category, value) values (\'' + interest.category + '\', \'' + interest.value + '\')' +
-            'RETURNING id INTO id'+interest.category+interest.value+';' +
-            'INSERT INTO UserInterest (idUser, idInterest) values (' + idUser + ', id'+interest.category+interest.value+' );' +
-            'END;';
+    function queryInsertInterestForUser(interest, idNewUser) {
+		return 'DECLARE idInterest int;'+
+		'BEGIN'+
+		'select id from Interest where value =\'' + interest.value + '\' and category = \'' + interest.category + '\' into idInterest;'+
+		'IF idInterest is NULL THEN'+
+			'insert into Interest(category, value) values(\'' + interest.category + '\',\'' + interest.value + '\') returning id into idInterest;' +
+		'END IF;'+
+		'insert into UserInterest (idUser,idInterest) values (' + idNewUser + ', idInterest);' +
+		'END;';
     }
 
     function queryInsertUserInterest(idUser, idInterest) {
@@ -21,7 +39,7 @@ module.exports = function() {
     function queryInsertUser(user) {
         return 'INSERT INTO UserProfile(name, alias, email, latitude, longitude) ' +
             'VALUES (\'' + user.name + '\', \'' + user.alias + '\', \'' + user.email + '\',' +
-            user.location.latitude + ',' + user.location.longitude + ');';
+            user.location.latitude + ',' + user.location.longitude + ')';
     }
 
     function querySelectLastUser(user) {
@@ -41,38 +59,25 @@ module.exports = function() {
         //First I check if the Interest already exists
         console.log('insert interest ' + JSON.stringify(interest));
         var idInterest = 0;
-        //var insertInterests = "";
         client.query(querSelectInterest(interest), function(err, result) {
             if (result.rows.length === 0) {
                 console.log('the interest didn\'t exist, i save it');
-                //insertInterests += queryInsertInterestForUser(interest,idUser);
-                console.log(queryInsertInterestForUser(interest, idUser));
                 client.query(queryInsertInterestForUser(interest, idUser), function(err) {
                     if (err) {
-                        console.log('hubo un erro ' + err);
+                        console.log('the interest didn\'t exist, i save it');
                         return false;
                     }
                 });
-
             } else {
                 console.log('interest id is \n' + result.rows[0]['id']);
                 idInterest = result.rows[0]['id'];
-                client.query(queryInsertUserInterest(idUser, idInterest), function(err) {
-                    if (err) {
-                        return false;
-                    }
-                    return true;
-                });
-                //insertInterests += queryInsertUserInterest(idUser, idInterest);
             }
-            /*
             client.query(queryInsertUserInterest(idUser, idInterest), function(err) {
                 if (err) {
                     return false;
                 }
                 return true;
             });
-            */
         });
     }
 
@@ -80,53 +85,32 @@ module.exports = function() {
 
     function postNewUser(request, response) {
 
-        var status = 400;
         var body = 'Error';
         //console.log(request.body);
         var user = request.body.user;
         console.log('user\n' + user);
         var interests = request.body.user.interests;
-
+        var query = queryInsertInterestForUser(user);
+        console.log(query);
         //var selectInterest = 'SELECT id from UserProfile where email = \'' + user.email + '\' ;';
         var idUser = 0;
 
         pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query('DO $$; BEGIN', function(err) {
-                if (err) return rollback(client, done, err, response, status, body);
-                client.query(queryInsertUser(user), function(err) {
-                    if (err) {
-                        return rollback(client, done, err, response, status, 'Error al guardar el usuario');
-                    }
-                    console.log('select user ');
-                    client.query(querySelectLastUser(user), function(err, result) {
-                        if (err || result.rows.length === 0) {
-                            return rollback(client, done, err, response, status, 'Error al guardar el usuario');
-                        }
-                        console.log('New User Id\n' + JSON.stringify(result));
-                        idUser = result.rows[0]['id'];
+            client.query(query, function(err) {
+                if (err) {
+                	body = {
+                		error: 'No se pudo guardar el usuario',
+                		metadata: request.body.metadata
+                	};
+                	response.status(400).send(body);	
+                }
+                console.log('Se guardo ok');
+                user.id = idUser;
 
-                        var interestsInserts = '';
-
-                        for (var i = 0; i < interests.length; i++) {
-                            var interest = interests[i];
-                            var ok = insertUserInterest(interest, idUser, client);
-                            console.log('status insert' + ok);
-                            if (ok === false) {
-                                console.log('Hubo un error');
-                                return rollback(client, done, err, response, status, 'Error al guardar los intereses');
-                            }
-                        }
-
-                        console.log('Se guardo ok va por el interes i', i);
-                        user.id = idUser;
-
-                        body = {};
-                        body.user = user;
-                        body.metadata = request.body.metadata;
-                        client.query('END $$', client.end.bind(client));
-                        response.status(201).send(body);
-                    });
-                });
+                body = {};
+                body.user = user;
+                body.metadata = request.body.metadata;
+                response.status(201).send(body);
             });
         });
     }
