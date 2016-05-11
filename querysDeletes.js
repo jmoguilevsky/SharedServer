ae407477d81267eb2280d49c6f5910b8da571abb
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-11 00:07:30 -0300

    delete de un user

--- querysDeletes.js
+++ querysDeletes.js
@@ -1,9 +1,11 @@
 module.exports = function () {
 	function queryDeleteUser (idUser) {
-		return 'delete from photo where idUser = '+ idUser+ ';' +
+		return 'BEGIN ' +
+			   'delete from photo where idUser = '+ idUser+ ';' +
 			   'delete from location where idUser = '+ idUser+ ';' +
 			   'delete from UserInterest where idUser = '+ idUser+ ';' +
-			   'delete from UserProfile where idUser = '+ idUser+ ';';
+			   'delete from UserProfile where idUser = '+ idUser+ ';' +
+			   'END;';
 	}
 
 	return{
