32f110c5ec43717e547ac5f5c6f8ab64946dc4a1
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-14 19:10:51 -0300

    primera interaccion angular y node

---
+++ views/controllers/usercontroller.js
@@ -0,0 +1,21 @@
+var userApp = angular.module('userApp', []);
+
+userApp.controller('UserListController',function ($scope, $http) {
+	/*
+	$http.get('/api/users')
+        .success(function(data) {
+            $scope.users = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+	*/
+	$scope.users = [{
+		"name":"joni",
+		"alias":"el mago wimo"
+	},{
+		"name":"manu",
+		"alias":"el doctor hibbert"
+	}];
+});
