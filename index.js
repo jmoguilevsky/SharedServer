3e01384ced4c7f9111d80414a704c9857904456d
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-14 01:30:31 -0300

    empiezo header y nav bar

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ router.route('/users/:idUser').get(getUser).put(putUpdateUser).delete(deleteUser
 router.route('/users/:idUser/photo').put(putUpdatePhoto);
 router.route('/interests').get(getInterests).post(postInterest);
 
-app.use(express.static(__dirname + '/public'), router);
+app.use(express.static(__dirname + '/public'));
