e08175a3974493a3f9a37f6837701fae73c8003e
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-14 00:17:58 -0300

    empiezo header y nav bar

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ app.set('port', (process.env.PORT || 5000));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
