482571872ded38512ff026085fc3eede42bb819c
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-14 19:13:42 -0300

    primera interaccion angular y node

--- views/controllers/usercontroller.js
+++ views/controllers/usercontroller.js
@@ -3,8 +3,8 @@ var userApp = angular.module('userApp', []);
 userApp.controller('UserListController',function ($scope, $http) {
 	$http.get('/users')
         .success(function(data) {
-            $scope.users = data;
-            console.log(data);
+            $scope.users = data.users;
+            console.log(data.users);
         })
         .error(function(data) {
             console.log('Error: ' + data);
