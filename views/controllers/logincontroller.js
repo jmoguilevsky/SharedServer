b3e240466dc12e42b907ca431ffd3ccf8f8952e0
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-19 00:32:09 -0300

    mock del login

--- views/controllers/logincontroller.js
+++ views/controllers/logincontroller.js
@@ -6,18 +6,18 @@ userApp.controller('LoginController',function ($scope, $http, $window) {
 	$scope.userForm = {};
 
 	$scope.login = function() {
-		$window.location.href = '/';
-		/*
+		console.log('try login '+ $scope.userForm);
+		
 		$http.post('https://enigmatic-depths-58073.herokuapp.com/login', $scope.userForm)
 		.success(function(data) {
 	            $scope.user = data;
 	            console.log(data);
-	            $location.path('/');
+	            $window.location.href = '/';
             })
 		.error(function(data) {
 			console.log('Error: ' + data);
 		});
-		*/
+		
 	};
 
 });
