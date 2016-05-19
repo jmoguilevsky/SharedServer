8c5eb936ff7d49b81a97490a8740fa54cbfafcf9
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-18 23:47:29 -0300

    mock del login

--- views/controllers/logincontroller.js
+++ views/controllers/logincontroller.js
@@ -8,8 +8,8 @@ userApp.controller('LoginController',function ($scope, $http) {
 	$scope.login = function() {
 		$http.post('https://enigmatic-depths-58073.herokuapp.com/login', $scope.userForm)
 		.success(function(data) {
-                $scope.user = data;
-                console.log(data);
+	            $scope.user = data;
+	            console.log(data);
             })
 		.error(function(data) {
 			console.log('Error: ' + data);
