52bb80495df12472012edb78657dadf84120755a
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-20 23:52:00 -0300

    nueva vista de usuarios

--- views/controllers/usercontroller.js
+++ views/controllers/usercontroller.js
@@ -6,7 +6,8 @@ userApp.controller('UserListController',function ($scope, $http) {
 	$http.get('/usersWithPhotos')
 		.success(function(data) {
 			$scope.users = data.users;
-			console.log("data" + data.users);
+			console.log("data ");
+			console.log(data.users);
 		})
 		.error(function(data) {
 			console.log('Error: ' + data);
