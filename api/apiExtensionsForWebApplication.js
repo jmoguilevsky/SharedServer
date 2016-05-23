4c45ec2df90a46317a410751c5a22045745ceefe
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-23 01:06:12 -0300

    empiezo login

--- api/apiExtensionsForWebApplication.js
+++ api/apiExtensionsForWebApplication.js
@@ -3,8 +3,9 @@ module.exports = function() {
     var version = 0.1;
 
     function login(request, response) {
-        console.log(request.body.email);
-        query = 'select * from UserProfile';
+        var email = request.body.email;
+        var password = request.body.password;
+        query = 'select * from UserProfile where email = \''+email+'\' and password = \''+password+'\';';
         pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             client.query(query, function(err, result) {
                 done();
@@ -13,6 +14,7 @@ module.exports = function() {
                     response.send("Error " + err);
                 } else {
                     //response.send(result.rows) ;
+                    console.log('count ' + result.rowCount);
                     console.log(result);
                     console.log('user');
                     response.status(200).send('ok');
