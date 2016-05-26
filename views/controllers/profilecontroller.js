217bb6d2eba0b65a23759c59844f314e0d380fea
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-26 00:50:22 -0300

    empiezo profile

--- views/controllers/profilecontroller.js
+++ views/controllers/profilecontroller.js
@@ -1,10 +1,8 @@
 var userApp = angular.module('userApp', []);
 
-userApp.controller('ProfileController',function ($scope, $http, $routeParams) {
-	$scope.user = {};
-
-	$scope.get('/getUser'{
-			"user": $routeParams.param1;
+userApp.controller('ProfileController',function ($scope, $http) {
+	$http.get('/getUser'{
+			"user": 1;
 		})
 	.success(function(data) {
 		$scope.user = data.user;
