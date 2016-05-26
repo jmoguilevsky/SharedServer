7ca1378fa991b8008cce594f9ccc99ac4c60d6b6
Jonathan Moguilevsky <jmoguilevsky@gmail.com>
2016-05-26 00:47:54 -0300

    empiezo profile

--- views/controllers/profilecontroller.js
+++ views/controllers/profilecontroller.js
@@ -3,16 +3,15 @@ var userApp = angular.module('userApp', []);
 userApp.controller('ProfileController',function ($scope, $http, $routeParams) {
 	$scope.user = {};
 
-	$scope.getProfile = function() {
-		console.log($routeParams.param1);
-		$http.post('/getUser', {
+	$scope.get('/getUser'{
 			"user": $routeParams.param1;
 		})
-		.success(function(data) {
-	            $scope.user = data;
-            })
-		.error(function(data) {
-			console.log('Error: ' + data);
-		});
-	};
+	.success(function(data) {
+		$scope.user = data.user;
+		console.log("data");
+		console.log(data.user);
+	})
+	.error(function(data) {
+		console.log('Error: ' + data);
+	});
 });
--- views/controllers/usercontroller.js
+++ views/controllers/usercontroller.js
@@ -4,13 +4,12 @@ var userApp = angular.module('userApp', []);
 userApp.controller('UserListController',function ($scope, $http) {
 	
 	$http.get('/usersWithPhotos')
-		.success(function(data) {
-			$scope.users = data.users;
-			console.log("data ");
-			console.log(data.users);
-		})
-		.error(function(data) {
-			console.log('Error: ' + data);
+	.success(function(data) {
+		$scope.users = data.users;
+		console.log("data ");
+		console.log(data.users);
+	})
+	.error(function(data) {
+		console.log('Error: ' + data);
 	});
-
 });
